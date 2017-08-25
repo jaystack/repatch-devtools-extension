2878aa3be88f935c12fac3247fe7486bf6c49ef0
merlinw <the.merlinw@gmail.com>
2017-08-25 18:05:26 +0200

    ul tree, css support

--- src/components/DiffViewer/getHtml.ts
+++ src/components/DiffViewer/getHtml.ts
@@ -2,7 +2,7 @@ import { Diff, DiffMap } from '../../types';
 
 export default function getHtml(diff: Diff, map: DiffMap, breaking: boolean): string {
   const ARROW = ' => ';
-  const TAGS = { ul: 'ul', li: 'li', div: 'div', strike: 'strike', pre: 'pre' };
+  const TAGS = { ul: 'ul', cul: 'cul', li: 'li', div: 'div', strike: 'strike', strong: 'strong', pre: 'pre' };
 
   let htmlData = '';
   let multilevelData = '';
@@ -10,15 +10,19 @@ export default function getHtml(diff: Diff, map: DiffMap, breaking: boolean): st
   function htmlTagger(tag, data) {
     switch (tag) {
       case TAGS.ul:
-        return '<ul>' + data + '</ul>';
+        return `<ul class="${TAGS.ul}">` + data;
+      case TAGS.cul:
+        return `</ul>`;
       case TAGS.li:
-        return '<li>' + data + '</li>';
+        return `<li class="${TAGS.li}">` + data + `</li>`;
       case TAGS.div:
-        return '<div>' + data + '</div>';
+        return `<div class="${TAGS.div}">` + data + `</div>`;
       case TAGS.strike:
-        return '<strike>' + data + '</strike>';
+        return `<span class="${TAGS.strike}"><strike>` + data + `</strike></span>`;
+      case TAGS.strong:
+        return `<span class="${TAGS.strong}"><strong>` + data + `</strong></span>`;
       case TAGS.pre:
-        return '<pre>' + data + '</pre>';
+        return `<pre class="${TAGS.pre}">` + data + `</pre>`;
       default:
         return;
     }
@@ -37,17 +41,31 @@ export default function getHtml(diff: Diff, map: DiffMap, breaking: boolean): st
             traverse(item);
           } else {
             multilevelData = '';
+
+            //path entries
             const changeObj = map.get(item);
             changeObj.path.map((elm) => {
-              return !!elm ? multilevelData = multilevelData.concat(htmlTagger(TAGS.li, elm)) : '';
+              return !!elm ? multilevelData = multilevelData.concat(htmlTagger(TAGS.ul, htmlTagger(TAGS.li, elm + ' {'))) : '';
             });
+
+            //object/string value handle
             const changeVal = typeof changeObj.rhs === 'object'
               ? htmlTagger(TAGS.pre, JSON.stringify(changeObj.rhs))
               : changeObj.rhs;
+
+            //value changes
             multilevelData = multilevelData.concat(
-              htmlTagger(TAGS.li, htmlTagger(TAGS.strike, changeObj.lhs) + ARROW + changeVal)
+              htmlTagger(TAGS.ul, htmlTagger(TAGS.li, htmlTagger(TAGS.strike, changeObj.lhs) + ARROW + htmlTagger(TAGS.strong, changeVal)))
             );
-            htmlData = htmlData.concat(htmlTagger(TAGS.ul, multilevelData));
+            multilevelData = multilevelData.concat(htmlTagger(TAGS.cul, multilevelData));
+
+            //ul closing
+            changeObj.path.map((elm) => {
+              return !!elm ? multilevelData = multilevelData.concat(' }' + htmlTagger(TAGS.cul, multilevelData)) : '';
+            });
+
+            // htmlData = htmlData.concat(htmlTagger(TAGS.ul, multilevelData));
+            htmlData = htmlData.concat(multilevelData);
           }
         }
       }
