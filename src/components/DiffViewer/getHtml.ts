b6c0c452c20bda6f71b65c5ad67907159f7133d7
merlinw <the.merlinw@gmail.com>
2017-08-29 17:17:16 +0200

    object tree

--- src/components/DiffViewer/getHtml.ts
+++ src/components/DiffViewer/getHtml.ts
@@ -1,78 +1,55 @@
 import { Diff, DiffMap } from '../../types';
 
+const ARROW = ' => ';
+let htmlData = '';
+
 export default function getHtml(diff: Diff, map: DiffMap, breaking: boolean): string {
-  const ARROW = ' => ';
-  const TAGS = { ul: 'ul', cul: 'cul', li: 'li', div: 'div', strike: 'strike', strong: 'strong', pre: 'pre' };
-
-  let htmlData = '';
-  let multilevelData = '';
-
-  function htmlTagger(tag, data) {
-    switch (tag) {
-      case TAGS.ul:
-        return `<ul>` + data;
-      case TAGS.cul:
-        return `</ul>`;
-      case TAGS.li:
-        return `<li>` + data + `</li>`;
-      case TAGS.div:
-        return `<div>` + data + `</div>`;
-      case TAGS.strike:
-        return `<span><strike>` + data + `</strike></span>`;
-      case TAGS.strong:
-        return `<span><strong>` + data + `</strong></span>`;
-      case TAGS.pre:
-        return `<pre>` + data + `</pre>`;
-      default:
-        return;
-    }
-  }
-
-  for (const val of Object.values(diff)) {
+  for (const [ key, val ] of Object.entries(diff)) {
     if (typeof(val) !== 'object') {
       const changeObj = map.get(val);
-      htmlData = htmlData.concat(
-        htmlTagger(TAGS.div, htmlTagger(TAGS.strike, changeObj.lhs) + ARROW + htmlTagger(TAGS.strong, changeObj.rhs))
-      );
+      htmlData = htmlData.concat(`<ul><li>${key}: <strike>${changeObj.lhs}</strike>${ARROW}<strong>${changeObj.rhs}</strong></li></ul>`);
     } else {
-      function traverse(o) {
-        for (const item of Object.values(o)) {
-          if (!!item && typeof(item) === 'object') {
-            traverse(item);
-          } else {
-            multilevelData = '';
-
-            //path entries
-            const changeObj = map.get(item);
-            changeObj.path.map((elm) => {
-              return !!elm ? multilevelData = multilevelData.concat(htmlTagger(TAGS.ul, htmlTagger(TAGS.li, elm + ' {'))) : '';
-            });
-
-            //object/string value handle
-            const changeVal = typeof changeObj.rhs === 'object'
-              ? htmlTagger(TAGS.pre, JSON.stringify(changeObj.rhs))
-              : changeObj.rhs;
-
-            //value changes
-            multilevelData = multilevelData.concat(
-              htmlTagger(TAGS.ul, htmlTagger(TAGS.li, htmlTagger(TAGS.strike, changeObj.lhs) + ARROW + htmlTagger(TAGS.strong, changeVal)))
-            );
-            multilevelData = multilevelData.concat(htmlTagger(TAGS.cul, multilevelData));
-
-            //ul closing
-            changeObj.path.map((elm) => {
-              return !!elm ? multilevelData = multilevelData.concat(' }' + htmlTagger(TAGS.cul, multilevelData)) : '';
-            });
-
-            // htmlData = htmlData.concat(htmlTagger(TAGS.ul, multilevelData));
-            htmlData = htmlData.concat(multilevelData);
-          }
-        }
-      }
-
-      traverse(val);
+      htmlData = htmlData.concat('<ul><li>' + key + '</li>');
+      traverse(val, diff, map);
     }
   }
 
   return htmlData;
 }
+
+
+function traverse(o, diff, map) {
+  if (Object.keys(o).length === 1) {
+    htmlData = htmlData.concat('<ul>');
+    for (const [ key, item ] of Object.entries(o)) {
+      if (typeof item !== 'object') {
+        const changeObj = map.get(item);
+        htmlData = htmlData.concat('<li>' + key + ': ' + getValue(changeObj) + '</li>');
+        htmlData = htmlData.concat('</ul>');
+      } else {
+        htmlData = htmlData.concat('<li>' + key + ' {</li>');
+        traverse(item, diff, map);
+        htmlData = htmlData.concat('} </ul>');
+      }
+    }
+
+  }
+
+  else if (Object.keys(o).length > 1) {
+    for (const [ key, item ] of Object.entries(o)) {
+      if (typeof item !== 'object') {
+        const changeObj = map.get(item);
+        htmlData = htmlData.concat('<ul><li>' + key + ': ' + getValue(changeObj) + '</li></ul>');
+      } else {
+        traverse(item, diff, map);
+      }
+    }
+    htmlData = htmlData.concat('} </ul>');
+  }
+}
+
+function getValue(changeObj) {
+  const changeVal = typeof changeObj.rhs === 'object' ? `<pre>${JSON.stringify(changeObj.rhs)}</pre>` : changeObj.rhs;
+  return `<strike>${changeObj.lhs}</strike>${ARROW}<strong>${changeVal}</strong>`;
+}
+
